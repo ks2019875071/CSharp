dbaede06f60c0da56d23d27fea8cd9505bb2a577
ks2019875071 <48702191+ks2019875071@users.noreply.github.com>
2021-03-15 23:09:44 +0900

    Add files via upload

---
+++ 0315/PropertyApp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class PropertyClass
+{
+	private int privateValue;
+	public int PrivateValue
+	{
+		get { return privateValue; }
+		set { privateValue = value; }
+		
+		public void PrintValue()
+	{
+		Console.WriteLine("Hidden Value = " + privateValue);
+	}
+}
+
+
+	class PropertyApp
+    {
+		public static void main()
+		{
+			int n;
+			PropertyClass obj = new PropertyClass();
+			obj.PrivateValue = 100; //invoke set-accessor
+			obj.PrintValue();
+
+			n = obj.PrivateValue; //invoke get-accessor
+			Console.WriteLine("Value = " + n);
+		}
+
+    }
+}
