9687f11717a5cc2807e200f795cce5b384003557
ks2019875071 <48702191+ks2019875071@users.noreply.github.com>
2021-03-29 22:42:10 +0900

    Add files via upload

---
+++ 0329/21ReadIntegerApp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0329
+{
+    class ReadIntegerApp
+    {
+        static int ReadInt()
+        {
+            char ch;
+            int n = 0;
+            
+            while (!char.IsDigit(ch = (char)Console.Read())) ; //when ch != digit
+
+            do
+            {
+                n = n * 10 + (ch - '0');
+                ch = (char)Console.Read();
+            } while (char.IsDigit(ch)); //when ch == digit
+            return n;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** input data : ");
+            Console.WriteLine("*** read data : " + ReadInt() + " " + ReadInt()); //digit 2개를 읽음
+        }
+    }
+}
