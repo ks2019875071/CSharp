9a1e4d2c996de416bea0d99a1035f98f9d881e35
ks2019875071 <48702191+ks2019875071@users.noreply.github.com>
2021-04-04 01:32:07 +0900

    Add files via upload

---
+++ 0403(6)/04Loop.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0403
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int i = 0;
+            //while (i<10)
+            // { 
+            //Console.WriteLine("{0}: Hello C#", i);
+            //i++;
+            //}
+
+            int j = 0;
+            do
+            {
+                Console.WriteLine("{0} : Hello C#", j);
+                j++;
+            } while (j < 10);
+
+            for (int k=0; k<10; k++)
+                Console.WriteLine("{0}: Hello C#", k);
+        }
+    }
+}
