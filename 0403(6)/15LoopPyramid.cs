9f137acb582b2df0464a8bb9c276174c58686489
ks2019875071 <48702191+ks2019875071@users.noreply.github.com>
2021-04-04 23:25:37 +0900

    Add files via upload

--- 0403(6)/15LoopPyramid.cs
+++ 0403(6)/15LoopPyramid.cs
@@ -36,6 +36,38 @@ namespace _0403
                 Console.WriteLine();
             }
             Console.WriteLine();
+
+            // (4)
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 5 - i; j++)
+                    Console.Write(" ");
+                for (int j = 1; j <= i; j++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            // (5)
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 5 - i; j++)
+                    Console.Write(" ");
+                for (int j = 1; j <= 2 * i - 1; j++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            // (6)
+            for (int i = 5; i >= 1; i--)
+            {
+                for (int j = 1; j <= 5 - i; j++)
+                    Console.Write(" ");
+                for (int j = 1; j <= 2 * i - 1; j++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
         }
     }
 }
