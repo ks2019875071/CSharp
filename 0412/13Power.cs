1407a2544997ef4e4bbb1f50f5f51d728644fd37
ks2019875071 <48702191+ks2019875071@users.noreply.github.com>
2021-04-12 22:53:49 +0900

    Add files via upload

---
+++ 0412/13Power.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace 0412
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			for (int i = 0; i <= 20; i++)
+				Console.WriteLine("2 ^ {0,2} = {1, 7}", i, Power(2, i));
+		}
+
+
+		private static int Power(int v, int i)
+		{
+			int p = 1;
+			for (int i = 1; i <= m; i++)
+				p *= n;
+
+			return p;
+		}
+	}
+}
