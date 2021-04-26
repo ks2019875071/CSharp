544b15e882cb12cbc77daffb3c9c93289ccdebf8
ks2019875071 <48702191+ks2019875071@users.noreply.github.com>
2021-04-26 22:45:15 +0900

    Add files via upload

---
+++ 0426/14NamespaceApp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using Shape;
+
+namespace Shape
+{
+    public class Rectangle
+    {
+        public int height;
+        public int width;
+        override public string ToString()
+        {
+            return string.Format("Width: {0}, Height: {1}", width, height);
+        }
+    }
+}
+
+namespace _0426
+{
+    class NamespaceApp
+    {
+        public static void Main()
+        {
+            Rectangle rect = new Rectangle();
+            rect.width = 10;
+            rect.height = 20;
+            Console.WriteLine("rect : " + rect);
+        }
+    }
+}
