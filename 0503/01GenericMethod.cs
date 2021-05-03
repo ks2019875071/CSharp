b365c62400dda7dd88369d481838215b7402dd48
ks2019875071 <48702191+ks2019875071@users.noreply.github.com>
2021-05-03 22:24:57 +0900

    Add files via upload

---
+++ 0503/01GenericMethod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0503
+{
+    class GenericMethod
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3 };
+            double[] d = { 0.1, 0.2, 0.3 };
+            string[] s = { "tiger", "lion", "zebra" };
+            PrintArray<int>(a);
+            PrintArray<double>(d);
+            PrintArray<string>(s);
+        }
+        
+        private static void PrintArray<T>(T[] a)
+        {
+            foreach (var item in a)
+                Console.Write("{0,8}", item);
+            Console.WriteLine();
+        }
+    }
+}
